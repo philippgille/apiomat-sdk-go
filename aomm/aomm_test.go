faec8ca4dc0252adb88301d3016df45edfc84686
Philipp Gille <philipp.gille@gmail.com>
2018-04-16 22:57:06 +0200

    Add simple test for aomm
    + Include the test in the CI build

---
+++ aomm/aomm_test.go
@@ -0,0 +1,22 @@
+package aomm
+
+import (
+	"testing"
+)
+
+func TestSystem(t *testing.T) {
+	cases := []struct {
+		sys  System
+		want string
+	}{
+		{Live, "LIVE"},
+		{Staging, "STAGING"},
+		{Test, "TEST"},
+	}
+	for _, c := range cases {
+		got := c.sys.String()
+		if got != c.want {
+			t.Errorf("got %q, want %q", got, c.want)
+		}
+	}
+}
