77479e625554cd5c6f1ebbf9ea12024078de3b47
Philipp Gille <philipp.gille@gmail.com>
2018-05-30 22:06:00 +0200

    Move converter functions into new file; add tests

--- aomc/class.go
+++ aomc/class.go
@@ -8,7 +8,6 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/philippgille/apiomat-go/aomc/dto"
-	"github.com/philippgille/apiomat-go/aoms"
 )
 
 // Class represents an ApiOmat class, also called "MetaModel".
@@ -154,67 +153,4 @@ func (client Client) GetAttributes(module string, classId string) ([]Attribute, 
 		result = append(result, elem)
 	}
 	return result, nil
-}
-
-// ConvertClassFromDto converts a raw class of type dto.Class into the convenience type aomc.Class.
-// It does NOT fetch and assign the class attributes, it just converts existing fields.
-func ConvertClassFromDto(rawClass dto.Class) Class {
-	result := Class{
-		// First go through all fields of the raw class that can be copied
-		AllowedRolesCreate:     rawClass.AllowedRolesCreate,
-		AllowedRolesGrant:      rawClass.AllowedRolesGrant,
-		AllowedRolesRead:       rawClass.AllowedRolesRead,
-		AllowedRolesWrite:      rawClass.AllowedRolesWrite,
-		Id:                     rawClass.ID,
-		IsGlobal:               rawClass.IsGlobal,
-		IsDeprecated:           rawClass.Deprecated,
-		IsInvisible:            rawClass.IsInvisible,
-		IsRestrictBinaryAccess: rawClass.RestrictResourceAccess,
-		IsTransient:            rawClass.IsTransient,
-		Name:                   rawClass.Name,
-		RequiredUserRoleCreate: rawClass.RequiredUserRoleCreate,
-		RequiredUserRoleGrant:  rawClass.RequiredUserRoleGrant,
-		RequiredUserRoleRead:   rawClass.RequiredUserRoleRead,
-		RequiredUserRoleWrite:  rawClass.RequiredUserRoleWrite,
-		UseOwnAuth:             rawClass.UseOwnAuth,
-	}
-	// Then those that need special attention
-	result.attributesHref = *aoms.MustUrl(url.Parse(rawClass.AttributesHref))
-	result.Created = time.Unix(0, rawClass.CreatedAt*int64(time.Millisecond))
-	result.Href = *aoms.MustUrl(url.Parse(rawClass.Href))
-	result.LastModified = time.Unix(0, rawClass.LastModifiedAt*int64(time.Millisecond))
-	result.methodsHref = *aoms.MustUrl(url.Parse(rawClass.MethodsHref))
-	result.moduleHref = *aoms.MustUrl(url.Parse(rawClass.ModuleHref))
-
-	return result
-}
-
-// ConvertAttributeFromDto converts a raw attribute of type dto.Attribute into the convenience type aomc.Attribute.
-func ConvertAttributeFromDto(rawAttribute dto.Attribute) Attribute {
-	result := Attribute{
-		// First go through all fields of the raw class that can be copied
-		IsDeprecated:      rawAttribute.Deprecated,
-		Id:                rawAttribute.ID,
-		IsBinary:          rawAttribute.Image,
-		IsCollection:      rawAttribute.IsCollection,
-		IsEmbedded:        rawAttribute.IsEmbeddedObject,
-		isFromOtherModule: rawAttribute.AddedFromOtherModule,
-		IsIgnored:         rawAttribute.JSONIgnore,
-		IsIndexed:         rawAttribute.IsIndexed,
-		IsMandatory:       rawAttribute.IsMandatory,
-		IsReadOnly:        rawAttribute.ReadOnly,
-		IsReference:       rawAttribute.IsReference,
-		IsSensitive:       rawAttribute.SensibleData,
-		Name:              rawAttribute.Name,
-		refID:             rawAttribute.RefID,
-		RoleForUpdate:     rawAttribute.RoleForUpdate,
-		Type:              rawAttribute.Type,
-	}
-	// Then those that need special attention
-	result.Created = time.Unix(0, rawAttribute.CreatedAt*int64(time.Millisecond))
-	result.Href = *aoms.MustUrl(url.Parse(rawAttribute.Href))
-	result.LastModified = time.Unix(0, rawAttribute.LastModifiedAt*int64(time.Millisecond))
-	result.metaModelHref = *aoms.MustUrl(url.Parse(rawAttribute.MetaModelHref))
-
-	return result
 }
---
+++ aomc/converter_test.go
@@ -0,0 +1,78 @@
+package aomc_test
+
+import (
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/go-test/deep"
+
+	"github.com/philippgille/apiomat-go/aomc"
+	"github.com/philippgille/apiomat-go/aomc/dto"
+)
+
+func TestConvertClassFromDto(t *testing.T) {
+	// Prep
+	// Not all fields must be tested, but one for each type.
+	// Slice of strings, time, URL, string, bool
+	msec := time.Now().UnixNano() >> 6
+	hrefString := "https://fake.url"
+	expectedUrl, _ := url.Parse(hrefString)
+	expected := aomc.Class{
+		AllowedRolesCreate: []string{
+			"someRole",
+		},
+		// Only actual time
+		Created:      time.Unix(0, msec*int64(time.Millisecond)),
+		Href:         *expectedUrl,
+		Id:           "123",
+		IsDeprecated: true,
+		// See above
+		LastModified: time.Unix(0, 0),
+	}
+	given := dto.Class{
+		AllowedRolesCreate: []string{
+			"someRole",
+		},
+		CreatedAt:  msec,
+		Href:       hrefString,
+		ID:         "123",
+		Deprecated: true,
+	}
+
+	// Test
+	actual := aomc.ConvertClassFromDto(given)
+	if diff := deep.Equal(expected, actual); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestConvertAttributeFromDto(t *testing.T) {
+	// Prep
+	// Not all fields must be tested, but one for each type.
+	// time, URL, string, bool
+	msec := time.Now().UnixNano() >> 6
+	hrefString := "https://fake.url"
+	expectedUrl, _ := url.Parse(hrefString)
+	expected := aomc.Attribute{
+		// Only actual time
+		Created:      time.Unix(0, msec*int64(time.Millisecond)),
+		Href:         *expectedUrl,
+		Id:           "123",
+		IsDeprecated: true,
+		// See above
+		LastModified: time.Unix(0, 0),
+	}
+	given := dto.Attribute{
+		CreatedAt:  msec,
+		Href:       hrefString,
+		ID:         "123",
+		Deprecated: true,
+	}
+
+	// Test
+	actual := aomc.ConvertAttributeFromDto(given)
+	if diff := deep.Equal(expected, actual); diff != nil {
+		t.Error(diff)
+	}
+}
